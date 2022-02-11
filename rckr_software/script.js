0bf7cb9d7b4b3fe1f3b364ede93edc66ac4627fe
Sayantika Ghosh <sayantikaghosh98@gmail.com>
2022-02-11 06:08:31 +0530

    Updated the logic found exclusive currency first and then population limit

--- rckr_software/script.js
+++ rckr_software/script.js
@@ -2,20 +2,18 @@ const dataFetch = async () => {
     const response = await fetch("https://cdn.jsdelivr.net/gh/apilayer/restcountries@3dc0fb110cd97bce9ddf27b3e8e1f7fbe115dc3c/src/main/resources/countriesV2.json");
     let data = await response.json();
 
-    const populationLimit = 61954;
+    const populationLimit = 840974;
 
 
 
-    data = data.filter(ele => ele.population >= populationLimit);
+    // data = data.filter(ele => ele.population >= populationLimit);
     const subData = [];
 
     data.forEach(ele => {
         const obj = {
             currencies : ele.currencies,
             population : ele.population,
-            area : ele.area,
             latlng : ele.latlng,
-            name : ele.name
         };
         subData.push(obj);
     });
@@ -40,21 +38,69 @@ const dataFetch = async () => {
 
     });
 
+    // console.log(finalCurrencyList);
+
+
+
+
+    const currencyCodeList = [];
+    subData.forEach(ele => {
+        ele.currencies.forEach(cur => {
+            currencyCodeList.push(cur.code);
+        });
+    });
+
+    const finalCurrencyCodeList = {};
+
+    currencyCodeList.forEach(ele => {
+        if(!finalCurrencyCodeList[ele]){
+            finalCurrencyCodeList[ele] = 0;
+        }
+        finalCurrencyCodeList[ele]++;
+
+    });
+
+
+
+
     const currencyNameList = [];
 
     Object.entries(finalCurrencyList).forEach(ele => {
-        if(ele[1] === 1 && ele[0] !== "[D]"){
+        if(ele[1] === 1){
             currencyNameList.push(ele[0]);
         }
     });
 
+    const currencyNameList1 = [];
+
+    Object.entries(finalCurrencyCodeList).forEach(ele => {
+        if(ele[1] === 1){
+            currencyNameList1.push(ele[0]);
+        }
+    });
+
+    const currencyCodeConvertedObj = {};
+    subData.forEach(ele => {
+        ele.currencies.forEach(cur => {
+            currencyCodeConvertedObj[cur.code] = cur.name;
+        });
+    });
+
+    const currencyNameList1FromCode = [];
+    currencyNameList1.forEach(ele => {
+        currencyNameList1FromCode.push(currencyCodeConvertedObj[ele]);
+    });
+
+    const currencyNameFinalList = currencyNameList.filter(ele => currencyNameList1FromCode.includes(ele));
+
 
     let subData1 = [];
-    currencyNameList.forEach(ele => {
+    currencyNameFinalList.forEach(ele => {
         if(!subData1.includes(subData.filter(item => item.currencies.find(cur => cur.name === ele))[0]))
-            subData1.push(subData.filter(item => item.currencies.find(cur => cur.name === ele))[0]);
-        
+            subData1.push(subData.filter(item => item.currencies.find(cur => cur.name === ele))[0]);    
     });
+
+    subData1 = subData1.filter(ele => ele.population >= populationLimit);
 
     function compare(a, b){
         if(a.population >= b.population) return 1;
@@ -62,7 +108,6 @@ const dataFetch = async () => {
     }
 
     subData1 = subData1.sort(compare);
-    // console.log(subData1);
 
     
 
@@ -97,6 +142,8 @@ const dataFetch = async () => {
 
         return (6371 * c);
     };
+
+    // console.log(calculateDistance([35.6544, 139.74477], [21.4225, 39.8261]));
 
     let distanceAns = 0;
 
